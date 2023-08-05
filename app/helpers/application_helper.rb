a0f6731652e26025b598afc6660776ae1a272b96
jiikko <n905i.1214@gmail.com>
2023-08-05 21:18:59 +0900

    helperに切り出す

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  # @param [CalendarDay, NilClass] calendar_day
+  def get_calendar_month_table_td_style(calendar_day)
+    return nil if calendar_day.nil?
+
+    if calendar_day.special_holiday?
+      'background-color:gray'
+    elsif calendar_day.not_scheduled_working_day?
+      'background-color:silver'
+    elsif calendar_day.worked?
+      'background-color:lime'
+    end
+  end
 end
