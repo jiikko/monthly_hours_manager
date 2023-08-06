efc7c6029eb17166a3f4582f46e8c425727d97b3
jiikko <n905i.1214@gmail.com>
2023-08-06 13:46:30 +0900

    ビューのロジックをhelperに移動する

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,14 @@ module ApplicationHelper
       'background-color:lime'
     end
   end
+
+  # @return [Boolean]
+  def can_render_hours_text_field?(calendar_day)
+    !calendar_day.special_holiday?
+  end
+
+  # @return [Boolean]
+  def can_render_special_holiday_checkbox?(calendar_day)
+    !calendar_day.worked?
+  end
 end
