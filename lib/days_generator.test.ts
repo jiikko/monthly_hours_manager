bc2e4e1140881bd0717e62619a8abdd8d5eee357
jiikko <n905i.1214@gmail.com>
2023-08-12 23:33:49 +0900

    test書く

--- lib/days_generator.test.ts
+++ lib/days_generator.test.ts
@@ -4,7 +4,9 @@ describe('DaysGenerator', () => {
   describe('execute', () => {
     describe('2023年8月の月火水が稼働日のとき', () => {
       it('配列を返す', () => {
-        console.log('hige');
+        const actual = DaysGenerator.execute(2023, 8, 84, { mon: true, tue: true, wed: true, thu: false, fri: false, sat: false, sun: false });
+        expect(actual[0]).toEqual({ day: 1, scheduled: 0, actual: 0 });
+        expect(actual[30]).toEqual({ day: 31, scheduled: 0, actual: 0 });
       })
     })
   })
