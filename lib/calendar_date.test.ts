df0790d598eabb2e3c9e568ed5517a94c7fff722
jiikko <n905i.1214@gmail.com>
2023-08-14 19:15:48 +0900

    test

--- lib/calendar_date.test.ts
+++ lib/calendar_date.test.ts
@@ -1,7 +1,7 @@
 import { CalendarDate } from './calendar_date';
 
 describe('CalendarDate', () => {
-  describe('引数に2023,08を与えるとき', () => {
+  describe('引数に2023,8,1を与えるとき', () => {
     it('new Date(2023, 7, 1)を作る', () => {
       const calendarDate = CalendarDate(2023, 8, 1);
       expect(calendarDate.year()).toBe(2023);
@@ -11,10 +11,11 @@ describe('CalendarDate', () => {
       expect(calendarDate.lastDayOfMonth()).toBe(31);
       expect(calendarDate.firstWeekDayOfMonth()).toBe(2);
       expect(calendarDate.monthlyKey()).toBe('2023-8');
+      expect(calendarDate.isNationalHoliday()).toBe(false);
     })
   })
 
-  describe('引数に2023,9を与えるとき', () => {
+  describe('引数に2023,9,1を与えるとき', () => {
     it('new Date(2023, 8, 1)を作る', () => {
       const calendarDate = CalendarDate(2023, 9, 1);
       expect(calendarDate.year()).toBe(2023);
@@ -24,10 +25,25 @@ describe('CalendarDate', () => {
       expect(calendarDate.lastDayOfMonth()).toBe(30);
       expect(calendarDate.firstWeekDayOfMonth()).toBe(5);
       expect(calendarDate.monthlyKey()).toBe('2023-9');
+      expect(calendarDate.isNationalHoliday()).toBe(false);
     })
   })
 
-  describe('引数に2023,11を与えるとき', () => {
+  describe('引数に2023,8,11を与えるとき', () => {
+    it('new Date(2023, 7, 11)を作る', () => {
+      const calendarDate = CalendarDate(2023, 8, 11);
+      expect(calendarDate.year()).toBe(2023);
+      expect(calendarDate.month()).toBe(8);
+      expect(calendarDate.day()).toBe(11);
+      expect(calendarDate.nextMonth()).toBe(9);
+      expect(calendarDate.lastDayOfMonth()).toBe(31);
+      expect(calendarDate.firstWeekDayOfMonth()).toBe(2);
+      expect(calendarDate.monthlyKey()).toBe('2023-8');
+      expect(calendarDate.isNationalHoliday()).toBe(true);
+    })
+  })
+
+  describe('引数に2023,11,1を与えるとき', () => {
     it('new Date(2023, 10, 1)を作る', () => {
       const calendarDate = CalendarDate(2023, 11, 1);
       expect(calendarDate.year()).toBe(2023);
@@ -37,11 +53,12 @@ describe('CalendarDate', () => {
       expect(calendarDate.lastDayOfMonth()).toBe(30);
       expect(calendarDate.firstWeekDayOfMonth()).toBe(3);
       expect(calendarDate.monthlyKey()).toBe('2023-11');
+      expect(calendarDate.isNationalHoliday()).toBe(false);
     })
   })
 
 
-  describe('引数に2023,12を与えるとき', () => {
+  describe('引数に2023,12,1を与えるとき', () => {
     it('new Date(2023, 11, 1)を作る', () => {
       const calendarDate = CalendarDate(2023, 12, 1);
       expect(calendarDate.year()).toBe(2023);
@@ -51,10 +68,11 @@ describe('CalendarDate', () => {
       expect(calendarDate.lastDayOfMonth()).toBe(31);
       expect(calendarDate.firstWeekDayOfMonth()).toBe(5);
       expect(calendarDate.monthlyKey()).toBe('2023-12');
+      expect(calendarDate.isNationalHoliday()).toBe(false);
     })
   })
 
-  describe('引数に2023,1を与えるとき', () => {
+  describe('引数に2023,1,1を与えるとき', () => {
     it('new Date(2023, 0, 1)を作る', () => {
       const calendarDate = CalendarDate(2023, 1, 1);
       expect(calendarDate.year()).toBe(2023);
@@ -64,6 +82,7 @@ describe('CalendarDate', () => {
       expect(calendarDate.lastDayOfMonth()).toBe(31);
       expect(calendarDate.firstWeekDayOfMonth()).toBe(0);
       expect(calendarDate.monthlyKey()).toBe('2023-1');
+      expect(calendarDate.isNationalHoliday()).toBe(true);
     })
   })
 
