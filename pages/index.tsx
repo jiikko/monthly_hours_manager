0ba8f3c5cfce22d7d2eedd8aca5996592152d678
jiikko <n905i.1214@gmail.com>
2023-08-20 12:10:03 +0900

    use layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,16 +3,21 @@ import styles from '../styles/Home.module.css';
 import Layout from '../components/layout';
 import { JsonParameter } from '../lib/json_parameter';
 import { useRouter } from 'next/router';
-import { useState, useEffect } from 'react';
 import { About } from '../components/about';
 
-export default function Page() {
+const Page: React.FC= ({}) => {
   const router = useRouter();
   const jsonObject = JsonParameter.parse(Object.fromEntries(Object.entries(router.query).map(([key, val]) => [key, String(val)])));
 
   return (
-    <Layout>
-      <About jsonObject={jsonObject} />
-    </Layout>
+    <About jsonObject={jsonObject} />
+  )
+}
+
+export default Page;
+
+Page.getLayout = function getLayout(page: React.ReactElement) {
+  return (
+    <Layout>{page}</Layout>
   )
 }
