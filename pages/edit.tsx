b3244691a9b3f9ad16cc05ef31c09a4be52ddeec
jiikko <n905i.1214@gmail.com>
2023-09-02 16:10:14 +0900

    使っていないimportを消す

--- pages/edit.tsx
+++ pages/edit.tsx
@@ -1,23 +1,19 @@
 import type { NextPageWithLayout } from './_app'
 import { Layout } from '../components/layout'
-import { useRouter } from 'next/router';
 import { Week } from '../lib/json_parameter';
 import { PathGenerator } from '../lib/path_generator';
 import { SettingForm } from '../components/setting_form';
-import { useEffect, useContext } from 'react';
 import { useCalendarState } from '../hooks/use_calendar_state';
-import { db } from "../lib/firebase";
-import { setDoc, doc } from "firebase/firestore";
 
 const Page: NextPageWithLayout = () => {
-  const { calendarState, dispatch, calendar, user } = useCalendarState(PathGenerator().editPath);
+  const { dispatch, calendar } = useCalendarState(PathGenerator().editPath);
   const handleSubmit = async (name: string, standardTime: number, week: Week, notify: (message: string) => void) => {
     dispatch({ type:'updateCalendar', payload: { name, standardTime, week }});
     notify('カレンダー情報の変更に成功しました。')
   }
 
   return (
-      <SettingForm calendar={calendar} handleSubmit={handleSubmit} />
+    <SettingForm calendar={calendar} handleSubmit={handleSubmit} />
   )
 }
 
