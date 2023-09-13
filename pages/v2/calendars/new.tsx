8fc5f78b4bc00683880eab8fcd57031a69c9d6e2
jiikko <n905i.1214@gmail.com>
2023-09-13 22:17:09 +0900

    calendarを作成する

--- pages/v2/calendars/new.tsx
+++ pages/v2/calendars/new.tsx
@@ -6,17 +6,24 @@ import { useReducer, useEffect, useContext, useState } from 'react';
 import { Week, Calendar } from '../../../lib/calendar';
 import { SettingForm } from '../../../components/setting_form';
 import { db } from "../../../lib/firebase";
-import { doc, getDoc, setDoc } from 'firebase/firestore';
+import { addDoc, runTransaction, collection } from 'firebase/firestore';
+import { useRouter } from 'next/router';
 
 const Page: NextPageWithLayout = () => {
+  const router = useRouter();
   const { user } = useContext(AuthContext);
-  const calendar = new Calendar('a', 11, {} as Week, {});
+  const calendar = new Calendar('a', 11, {} as Week, {}); // TODO: 仮の値
+
   const handleSubmit = async (name: string, standardTime: number, week: Week, notify: (message: string) => void) => {
-      const uid = user.uid;
-      const docRef = doc(db, `time-manager-v2/${uid}`);
-      setDoc(docRef, {
-        name: calendar.name, standardTime: calendar.standardTime, week: calendar.week, months: (calendar.months || {})
-      });
+    const newEntryPath = `time-manager-v2/${user.uid}/calendars`;
+    const docRef = await addDoc(collection(db, newEntryPath), {
+      name,
+      standardTime,
+      week,
+      months: {}
+    });
+    router.push(`/time_manager/v2/calendars`, undefined,{ scroll: false })
+    console.log('created', docRef.id)
   }
 
   return (
