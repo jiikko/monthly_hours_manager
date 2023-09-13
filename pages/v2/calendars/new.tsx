617c28f1d2942d3b5039de309dff2e446e3bdb99
jiikko <n905i.1214@gmail.com>
2023-09-13 20:55:40 +0900

    呼び出しを適当に書く

--- pages/v2/calendars/new.tsx
+++ pages/v2/calendars/new.tsx
@@ -1,12 +1,22 @@
 import type { NextPageWithLayout } from './../../_app'
 import { Layout } from '../../../layouts/v2';
 import { Button, Row, Col } from 'react-bootstrap';
-import { Calendar } from '../../../lib/calendar';
+import { AuthContext} from '../../../contexts/auth_context'
+import { useReducer, useEffect, useContext, useState } from 'react';
+import { Week, Calendar } from '../../../lib/calendar';
 import { SettingForm } from '../../../components/setting_form';
+import { db } from "../../../lib/firebase";
+import { doc, getDoc, setDoc } from 'firebase/firestore';
 
 const Page: NextPageWithLayout = () => {
-  const calendar = new Calendar('a', 11, {}, {});
+  const { user } = useContext(AuthContext);
+  const calendar = new Calendar('a', 11, {} as Week, {});
   const handleSubmit = async (name: string, standardTime: number, week: Week, notify: (message: string) => void) => {
+      const uid = user.uid;
+      const docRef = doc(db, `time-manager-v2/${uid}`);
+      setDoc(docRef, {
+        name: calendar.name, standardTime: calendar.standardTime, week: calendar.week, months: (calendar.months || {})
+      });
   }
 
   return (
