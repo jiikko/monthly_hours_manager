06cf845abd6acb5eeff51c35dc6dcc5b9dc7d0a9
jiikko <n905i.1214@gmail.com>
2023-09-16 15:34:55 +0900

    stop warning

--- components/setting_form.tsx
+++ components/setting_form.tsx
@@ -42,13 +42,13 @@ export const SettingForm: React.FC<Props> = ({ calendar, handleSubmit, submitLab
         </Form.Group>
 
         <Form.Label>稼働曜日</Form.Label>
-        <Form.Check type="switch" id='m' name="mon" label="月" className='mb-2' checked={workingWeek.mon} onChange={handleWorkingDaysChange} />
-        <Form.Check type="switch" id='t' name="tue" label="火" className='mb-2' checked={workingWeek.tue} onChange={handleWorkingDaysChange} />
-        <Form.Check type="switch" id='w' name="wed" label="水" className='mb-2' checked={workingWeek.wed} onChange={handleWorkingDaysChange} />
-        <Form.Check type="switch" id='thu' name="thu" label="木" className='mb-2' checked={workingWeek.thu} onChange={handleWorkingDaysChange} />
-        <Form.Check type="switch" id='fri' name="fri" label="金" className='mb-2' checked={workingWeek.fri} onChange={handleWorkingDaysChange} />
-        <Form.Check type="switch" id='sat' name="sat" label="土" className='mb-2' checked={workingWeek.sat} onChange={handleWorkingDaysChange} />
-        <Form.Check type="switch" id='sun' name="sun" label="日" className='mb-4' checked={workingWeek.sun} onChange={handleWorkingDaysChange} />
+        <Form.Check type="switch" id='m' name="mon" label="月" className='mb-2' checked={workingWeek.mon || false} onChange={handleWorkingDaysChange} />
+        <Form.Check type="switch" id='t' name="tue" label="火" className='mb-2' checked={workingWeek.tue || false} onChange={handleWorkingDaysChange} />
+        <Form.Check type="switch" id='w' name="wed" label="水" className='mb-2' checked={workingWeek.wed || false} onChange={handleWorkingDaysChange} />
+        <Form.Check type="switch" id='thu' name="thu" label="木" className='mb-2' checked={workingWeek.thu || false} onChange={handleWorkingDaysChange} />
+        <Form.Check type="switch" id='fri' name="fri" label="金" className='mb-2' checked={workingWeek.fri || false} onChange={handleWorkingDaysChange} />
+        <Form.Check type="switch" id='sat' name="sat" label="土" className='mb-2' checked={workingWeek.sat || false} onChange={handleWorkingDaysChange} />
+        <Form.Check type="switch" id='sun' name="sun" label="日" className='mb-4' checked={workingWeek.sun || false} onChange={handleWorkingDaysChange} />
 
         <Button variant="primary" type="submit" onClick={(_) => handleSubmit(name, standardTime, workingWeek)}>
           {submitLabel}
