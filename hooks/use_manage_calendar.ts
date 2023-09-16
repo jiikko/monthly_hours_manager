2c8a8a2d0881aa5410f4dcd948250743c856f2f1
jiikko <n905i.1214@gmail.com>
2023-09-16 20:59:44 +0900

    取得と書き込みの処理をhookとして切り出す

---
+++ hooks/use_manage_calendar.ts
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react';
+import { Calendar } from 'lib/calendar';
+import { db } from "lib/firebase";
+import { getDoc, doc, updateDoc, runTransaction, collection, query } from 'firebase/firestore';
+
+export const useManageCalendar = () => {
+  const [calendar, setCalendar] = useState<Calendar>(undefined);
+  // NOTE: calendar.monthsを更新した時に再レンダリングしてくれないので対策のためのstate. useReducerに変更すれば不要らしい
+  const [_, setCalendarMonth] = useState(null);
+
+  const updateCalendarForReRender = (calendar: Calendar, monthKey: string) => {
+    setCalendar(calendar)
+    if(calendar.months) { setCalendarMonth(calendar.months[monthKey]) }
+  }
+
+  const updateMonths = async (user: any, calendar_id: string, months: any, monthKey: string) => {
+    const entryPath = user && `time-manager-v2/${user.uid}/calendars/${calendar_id}`;
+    const docRef = doc(db, entryPath);
+    await updateDoc(docRef, { months: calendar.months });
+    updateCalendarForReRender(calendar, monthKey);
+  }
+
+  const fetchSingleCalendar = async (user: any, calendar_id: string, monthKey: string) => {
+    if (!user) return;
+
+    const entryPath = user && `time-manager-v2/${user.uid}/calendars/${calendar_id}`;
+    const docRef = doc(db, entryPath);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      console.log("Document data:", docSnap.data());
+      const doc = docSnap.data()
+      const calendar = new Calendar(doc.name, doc.standardTime, doc.week, doc.months, false, docSnap.id)
+      updateCalendarForReRender(calendar, monthKey);
+    } else {
+      setCalendar(null);
+      console.log("No such document!");
+    }
+  };
+
+  return { calendar, fetchSingleCalendar, updateMonths };
+}
--- pages/v2/calendars/[calendar_id]/[year]/[month]/index.tsx
+++ pages/v2/calendars/[calendar_id]/[year]/[month]/index.tsx
@@ -2,56 +2,28 @@ import type { NextPageWithLayout } from 'pages/_app'
 import { Layout } from 'layouts/v2';
 import { useEffect, useContext, useState } from 'react';
 import { AuthContext } from 'contexts/auth_context'
-import { db } from "lib/firebase";
-import { getDoc, doc, updateDoc, runTransaction, collection, query } from 'firebase/firestore';
-import { Calendar } from 'lib/calendar';
 import { CalendarDate } from 'lib/calendar_date';
-import Link from 'next/link';
 import { CalendarMonth } from 'components/calendar_month';
 import { useRouter } from 'next/router';
 import { DayObject } from 'lib/days_generator';
 import { MonthSummary } from 'components/month_summary';
 import { DaysGenerator } from 'lib/days_generator';
 import { toast } from 'react-toastify';
+import { useManageCalendar } from 'hooks/use_manage_calendar';
 
 const Page: NextPageWithLayout = () => {
   const router = useRouter();
   const { year, month } = router.query;
   const calendar_id = typeof router.query.calendar_id === 'string' ? router.query.calendar_id : null;
   const { user } = useContext(AuthContext);
-  const entryPath = user && `time-manager-v2/${user.uid}/calendars/${calendar_id}`;
-  const [calendar, setCalendar] = useState<Calendar>(null);
-  const [_, setCalendarMonth] = useState(null); // NOTE: calendar.monthsを更新した時に再レンダリングしてくれないので対策のstate. useReducerに変更すれば不要
   const date = CalendarDate(year && Number(year), month && Number(month), 1);
   const monthKey = date.monthlyKey();
-
-  const updateCalendar = (calendar: Calendar) => {
-    setCalendar(calendar)
-    if(calendar.months) {
-      setCalendarMonth(calendar.months[monthKey])
-    }
-  }
-
-  const fetchCalendar = async () => {
-    const docRef = doc(db, entryPath);
-    const docSnap = await getDoc(docRef);
-    if (docSnap.exists()) {
-      console.log("Document data:", docSnap.data());
-      const doc = docSnap.data()
-      const calendar = new Calendar(doc.name, doc.standardTime, doc.week, doc.months, false, docSnap.id)
-      updateCalendar(calendar)
-    } else {
-      // TODO: 404処理
-      console.log("No such document!");
-    }
-  }
+  const { fetchSingleCalendar, calendar, updateMonths } = useManageCalendar();
 
   const handleUpdateDay = async (attributeName: string, value: boolean | string, dayIndex: number): Promise<void> => {
     days[dayIndex][attributeName] = value;
     calendar.months[monthKey] = days.map((day: DayObject) => { return(day.toObject()) });
-    const docRef = doc(db, entryPath);
-    updateDoc(docRef, { months: calendar.months });
-    updateCalendar(calendar);
+    updateMonths(user, calendar_id, calendar.months, monthKey);
     toast('時間を更新しました');
   }
 
@@ -59,14 +31,12 @@ const Page: NextPageWithLayout = () => {
     const days = DaysGenerator.execute(Number(year), Number(month), calendar.standardTime, calendar.week);
     if(calendar.months === undefined) { calendar.months = {} }
     calendar.months[monthKey] = days;
-    const docRef = doc(db, entryPath);
-    await updateDoc(docRef, { months: calendar.months });
-    updateCalendar(calendar)
+    updateMonths(user, calendar_id, calendar.months, monthKey);
     toast('初期化しました');
   };
 
   useEffect(() => {
-    if(user) { fetchCalendar() }
+    fetchSingleCalendar(user, calendar_id, monthKey)
   }, [user])
 
   useEffect(() => {
@@ -83,16 +53,21 @@ const Page: NextPageWithLayout = () => {
   }, [user, calendar]);
 
   if(!user) { return null }
-  if(!calendar) { return null }
-  if(calendar.months && calendar.months[monthKey] === undefined) { return }
-  if(calendar.hasNoSetting()) {
+  if(calendar === undefined) { return null }
+  if(calendar === null) {
+    return(
+      <div className="alert alert-danger" role="alert">カレンダーが見つかりませんでした。</div>
+    )
+  }
+  if(calendar?.months && calendar.months[monthKey] === undefined) { return }
+  if(calendar?.hasNoSetting()) {
     return(
       <div className="alert alert-danger" role="alert">カレンダーの設定情報がありません。設定してください。</div>
     )
   }
 
   let days = []
-  if(calendar.months && calendar.months[monthKey]) {
+  if(calendar?.months && calendar.months[monthKey]) {
     days = calendar.months[monthKey].map((day: DayObject, _: number) => { return(new DayObject(day.scheduled, day.actual, day.day, day.isHoliday)) })
   } else {
     return null
