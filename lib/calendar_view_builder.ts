b90bf0cf9c5d5eaeec6f602d6d31708de23e53db
jiikko <n905i.1214@gmail.com>
2023-09-24 00:50:02 +0900

    annotate

--- lib/calendar_view_builder.ts
+++ lib/calendar_view_builder.ts
@@ -1,7 +1,7 @@
 import { CalendarDate } from 'lib/calendar_date';
 
 export const CalendarViewBuilder = (() => {
-  return function (handleUpdateDay, year, month) {
+  return function (handleUpdateDay: (attributeName: string, value: boolean | string, dayIndex: number) => void, year: number, month: number) {
     const headerWeeks = () => {
       return(
         ['日', '月', '火', '水', '木', '金', '土']
@@ -13,10 +13,10 @@ export const CalendarViewBuilder = (() => {
       const firstDayOfMonth = date.firstWeekDayOfMonth(); // 当月の最初の曜日を取得
       const daysInMonth = date.lastDayOfMonth(); // 当月の最終日の日付を取得
       const calendarInfo = generateMonthDays(firstDayOfMonth, daysInMonth);
-      return generateWeekRows(calendarInfo, handleUpdateDay, year, month);
+      return generateWeekRows(calendarInfo, handleUpdateDay);
     }
 
-    const generateMonthDays = (firstDayOfMonth, daysInMonth): Array<{ dayNumber: number | null }> => {
+    const generateMonthDays = (firstDayOfMonth: number, daysInMonth: number): Array<{ dayNumber: number | null }> => {
       const totalDays = 6 * 7; // 最大6週間 x 7日
       let dayCount = 1;
       const calendarInfo = Array.from({ length: totalDays }, (_, index) => {
@@ -29,10 +29,10 @@ export const CalendarViewBuilder = (() => {
 
       return calendarInfo as any;
     };
-    const generateWeekRows = (calendarInfo, handleUpdateDay, year, month) => {
+    const generateWeekRows = (calendarInfo, handleUpdateDay) => {
       const calendarRows = [];
       for (let i = 0; i < calendarInfo.length; i += 7) {
-        const week = calendarInfo.slice(i, i + 7).map(({ dayNumber }, index) => {
+        const week = calendarInfo.slice(i, i + 7).map(({ dayNumber }, index: number) => {
           return { dayNumber, index };  // 必要な情報だけを返す
         });
         calendarRows.push(week);
