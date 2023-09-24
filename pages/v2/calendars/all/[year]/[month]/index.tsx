42226b225aaeddd9b0cfe642576508a5b1a2b8d2
jiikko <n905i.1214@gmail.com>
2023-09-24 21:27:16 +0900

    anyをやめる

--- pages/v2/calendars/all/[year]/[month]/index.tsx
+++ pages/v2/calendars/all/[year]/[month]/index.tsx
@@ -1,10 +1,10 @@
 import {AllCalendarMonth} from 'components/all_calendar_month';
+import {useCalendarCollection} from 'hooks/use_calendar_collection';
 import {RequiredCalendarCollection} from 'layouts/required_calendar_collection';
 import {RequiredUser} from 'layouts/required_user';
 import {Layout} from 'layouts/v2';
 import {useRouter} from 'next/router';
 import type {NextPageWithLayout} from 'pages/_app';
-import {useCalendarCollection} from 'hooks/use_calendar_collection';
 
 const Page: NextPageWithLayout = () => {
   const router = useRouter();
