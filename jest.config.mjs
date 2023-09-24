0b1d0746de1af51bf64a5f8431beb0b32f4a8f9c
jiikko <n905i.1214@gmail.com>
2023-09-24 23:34:17 +0900

    jestの設定でパスの読み方を変える

--- jest.config.mjs
+++ jest.config.mjs
@@ -9,6 +9,9 @@ const createJestConfig = nextJest({
 // Add any custom config to be passed to Jest
 const customJestConfig = {
   testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^lib/(.*)$': '<rootDir>/lib/$1',
+  },
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
