0ab9d59730bc40400f07bf089ec28c98aca258fc
jiikko <n905i.1214@gmail.com>
2024-05-15 19:27:21 +0900

    最新の月を指すカレンダーの月URLを追加する

---
+++ pages/v2/calendars/[calendar_id]/latest.tsx
@@ -0,0 +1,38 @@
+import { RequiredCalendar } from "layouts/required_calendar";
+import { RequiredUser } from "layouts/required_user";
+import { Layout } from "layouts/v2";
+import { Today } from "lib/calendar_date";
+import { GetServerSideProps } from "next";
+import type { NextPageWithLayout } from "pages/_app";
+
+const Page: NextPageWithLayout = () => {
+  // コンポーネントはリダイレクトが行われるため実際にはレンダリングされません
+  return <div>Redirecting...</div>;
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const todayDate = Today();
+  const year = todayDate.year();
+  const month = todayDate.month();
+
+  const { calendar_id } = context.params; // [calendar_id] のパラメータを取得
+
+  return {
+    redirect: {
+      destination: `/v2/calendars/${calendar_id}/${year}/${month}`,
+      permanent: false,
+    },
+  };
+};
+
+Page.getLayout = function getLayout(page: React.ReactElement) {
+  return (
+    <Layout>
+      <RequiredUser>
+        <RequiredCalendar>{page}</RequiredCalendar>
+      </RequiredUser>
+    </Layout>
+  );
+};
+
+export default Page;
