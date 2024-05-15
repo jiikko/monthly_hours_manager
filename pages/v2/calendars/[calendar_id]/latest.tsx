be42c0a8d4ee78470c02b562fb96896f5f425826
jiikko <n905i.1214@gmail.com>
2024-05-15 21:07:00 +0900

    Use SSG

--- pages/v2/calendars/[calendar_id]/latest.tsx
+++ pages/v2/calendars/[calendar_id]/latest.tsx
@@ -2,27 +2,39 @@ import { RequiredCalendar } from "layouts/required_calendar";
 import { RequiredUser } from "layouts/required_user";
 import { Layout } from "layouts/v2";
 import { Today } from "lib/calendar_date";
-import { GetServerSideProps } from "next";
-import type { NextPageWithLayout } from "pages/_app";
+import type { GetStaticProps, NextPage } from "next";
+import { ReactElement, ReactNode } from "react";
+
+type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
 
 const Page: NextPageWithLayout = () => {
   return <div>Redirecting...</div>;
 };
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
+export const getStaticProps: GetStaticProps = async (context) => {
   const todayDate = Today();
   const year = todayDate.year();
   const month = todayDate.month();
 
-  const { calendar_id } = context.params;
+  const { calendar_id } = context.params as { calendar_id: string };
 
   return {
     redirect: {
       destination: `/v2/calendars/${calendar_id}/${year}/${month}`,
       permanent: false,
     },
+    revalidate: 60,
   };
 };
+
+export async function getStaticPaths() {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+}
 
 Page.getLayout = function getLayout(page: React.ReactElement) {
   return (
